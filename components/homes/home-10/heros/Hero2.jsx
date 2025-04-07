dee4c54d65e9b2f113a705d8d7c72c33f54b6691
pavann06 <pavan.06.p@gmail.com>
2025-04-07 12:57:45 +0530

    ukrane

--- components/homes/home-10/heros/Hero2.jsx
+++ components/homes/home-10/heros/Hero2.jsx
@@ -51,7 +51,7 @@ export default function Hero2() {
       {/* End Scroll Down */}
       {/* Status */}
       <div className="hs-status uppercase wow fadeInUp" data-wow-offset={0}>
-        Based in Kyiv, Ukraine
+      
       </div>
       {/* End Status */}
     </div>
